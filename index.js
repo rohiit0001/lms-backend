75a2f38df0311cb4dc4352e894948359bcea2fb6
nairx <nairxprv@gmail.com>
2026-02-25 14:54:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dbConnect from "./config/db.js";
+import userRouter from "./routers/userRoute.js";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(cors());
+const startServer = async () => {
+  await dbConnect();
+  app.listen(8080, () => console.log("Server Started"));
+};
+startServer()
+
+app.use("/api/users", userRouter);
